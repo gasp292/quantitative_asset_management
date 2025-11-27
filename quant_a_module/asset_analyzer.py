ce778bd654c720100f53c2dcb4f2f7f030fbbe99
GuillaumeTurlan <guillaume.turlan.05@gmail.com>
2025-11-27 21:23:19 +0100

    Ajout de la fonction run_strategy dans la classe Asset_analyzer

--- quant_a_module/asset_analyzer.py
+++ quant_a_module/asset_analyzer.py
@@ -19,11 +19,37 @@ class AssetAnalyzer():
         self.data = df
 
     def run_strategy(self,strategy_name, short_window,long_window):
-        return None
+        if self.data is None or self.data.empty :
+            return None
+        df = self.data.copy()
+
+        if strategy_name == "Buy and Hold":
+            df['Strategy_Retuns'] = self.data['Returns']
+
+        elif strategy_name == "Momentum":
+            # .rolling(X).mean() compute the mean of the X last days
+            df['SMA_Short'] = df['Close'].rolling(window=short_window).mean()
+            df['SMA_Long'] = df['Close'].rolling(window=long_window).mean()
+
+            df['Signal'] = np.where(df['SMA_Short'] > df ['SMA_Long'], 1, 0)
+            #strategy_return = signal of day n-1 * return of day n 
+            df['Strategy_Returns'] = df['Signal'].shift(1) * df['Returns']
+
+        df['Cumulative_strategy'] = (1 + df['Strategy_Returns'].fillna(0)).cumprod() * 100
+        return df
+    
 
 
 # ----------------------------------- Test of AssetAnalyzer Class ---------------------------------------
-Test = AssetAnalyzer('BTC-USD')
+
+Test = AssetAnalyzer('GOOG')
 Test.get_data()
-print(Test.data['Returns'].head())
-print(Test.data['Close'].head())
+
+df_result = Test.run_strategy("Momentum",20,50)
+cols_to_check = ['Close', 'SMA_Short', 'SMA_Long', 'Signal', 'Strategy_Returns']
+print(df_result[cols_to_check].tail(30))
+
+last_signal = df_result['Signal'].iloc[-1]
+print("Signal for today : ", last_signal, "| 1 = Buy, 0 = Sell")
+
+
